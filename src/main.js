d3e4df82406bcfaa00f219e370b6941fddeb2b41
Vic <waleria.tuszkowa@gmail.com>
2025-08-02 21:10:42 +0200

    main changed

--- src/main.js
+++ src/main.js
@@ -2,25 +2,30 @@ import { getImagesByQuery } from './js/pixabay-api';
 import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import axios from 'axios';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
+
   const query = form.elements['search-text'].value.trim();
   if (!query) {
-    iziToast.warning({ title: 'Warning', message: 'Please enter a search term', position: 'topRight' });
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search term',
+      position: 'topRight',
+    });
     return;
   }
 
   clearGallery();
   showLoader();
 
-  try {npm
+  try {
     const data = await getImagesByQuery(query);
     const hits = data.hits;
+
     if (hits.length === 0) {
       iziToast.info({
         title: 'No results',
@@ -32,7 +37,11 @@ form.addEventListener('submit', async event => {
     }
   } catch (error) {
     console.error(error);
-    iziToast.error({ title: 'Error', message: 'Something went wrong. Try again later.', position: 'topRight' });
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Try again later.',
+      position: 'topRight',
+    });
   } finally {
     hideLoader();
     form.reset();
